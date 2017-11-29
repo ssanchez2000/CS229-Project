8157abde3a9d506e3a3bfb0acc58de3eb032d11f
vishalsubbiah <subbiahvishal@gmail.com>
2017-11-29 22:41:15 +0000

    base works

--- Vishal_tests/base/smile_model.py
+++ Vishal_tests/base/smile_model.py
@@ -214,7 +214,7 @@ model.train()
 loss_fn = nn.CrossEntropyLoss().type(dtype)
 optimizer = optim.Adam(model.parameters(), lr=5e-5,weight_decay=0)
 print("start training")
-loss_history,acc_history,val_acc_history=train(train_loader,val_loader, model, loss_fn, optimizer, dtype,num_epochs=15, print_every=4)
+loss_history,acc_history,val_acc_history=train(train_loader,val_loader, model, loss_fn, optimizer, dtype,num_epochs=1, print_every=4)
 
 plt.plot(range(len(loss_history)),loss_history)
 plt.xlabel("iterations")
