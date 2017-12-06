b13b831e6311e01cac70b5ff98a2752ded23c51c
nickj <nickj@stanford.edu>
2017-12-06 02:47:10 +0000

    Improving val acc

--- Nick_tests/replicate/gender_model.py
+++ Nick_tests/replicate/gender_model.py
@@ -234,7 +234,7 @@ print("defined model")
 model.type(dtype)
 model.train()
 loss_fn = nn.CrossEntropyLoss().type(dtype)
-optimizer = optim.Adam(model.parameters(), lr=5e-5,weight_decay=5e-2)
+optimizer = optim.Adam(model.parameters(), lr=5e-5,weight_decay=1e-1)
 print("start training")
 loss_history,acc_history,val_acc_history=train(train_loader,val_loader, model, loss_fn, optimizer, dtype,num_epochs=15, print_every=17)
 
