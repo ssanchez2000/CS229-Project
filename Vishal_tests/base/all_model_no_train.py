ffa64facc7fa87b577479423507b095932c2c802
Vishal Subbiah <vishalsubbiah@Vishals-MacBook-Pro.local>
2017-12-06 20:24:20 -0800

    all model attempt 2

--- Vishal_tests/base/all_model_no_train.py
+++ Vishal_tests/base/all_model_no_train.py
@@ -378,7 +378,7 @@ loss_fn = nn.CrossEntropyLoss().type(dtype)
 all_optimizer = optim.Adam(all_model.parameters(), lr=5e-2)
 
 print("start training")
-loss_all_history, loss_gender_history,loss_smile_history, acc_all_history, acc_gender_history,acc_smile_history=all_train(train_loader, all_model,gender_model,smile_model, loss_fn, all_optimizer, dtype,num_epochs=1, print_every=10)
+loss_all_history, loss_gender_history,loss_smile_history, acc_all_history, acc_gender_history,acc_smile_history=all_train(train_loader, all_model,gender_model,smile_model, loss_fn, all_optimizer, dtype,num_epochs=15, print_every=10)
 
 plt.plot(range(len(loss_smile_history)),loss_smile_history)
 plt.xlabel("iterations")
